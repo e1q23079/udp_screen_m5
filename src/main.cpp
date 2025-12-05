aeb5da4eec9762204f5bd73d81afcb3e0b39ef87
e1q23079 <e1q23079@oit.ac.jp>
2025-12-05 13:40:06 +0900

    M5Unifiedライブラリを追加し、UDP受信機能を実装しました

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include <M5Unified.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char SSID[] = "udp_screen";
+const char PASSWORD[] = "samplePass";
+
+// UDP設定
+WiFiUDP wifiudp;
+const int port = 3000;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // セットアップ
+    auto cfg = M5.config();
+    M5.begin(cfg);
+    // アクセスポイント設定
+    WiFi.softAP(SSID, PASSWORD);
+    // 画面
+    M5.Display.setRotation(1);
+    M5.Display.setTextSize(2);
+    M5.Display.println("receiver");
+    M5.Display.printf("SSID:%s\n", SSID);
+    M5.Display.printf("PASSWORD:%s\n", PASSWORD);
+    M5.Display.println(WiFi.softAPIP());  // ホストIPアドレス
+
+    pinMode(10, OUTPUT);     // LEDピン設定
+    digitalWrite(10, HIGH);  // LED_OFF
+
+    wifiudp.begin(port);  // UDP開始
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    int packetSize = wifiudp.parsePacket();
+    if (packetSize) {
+        // digitalWrite(10, LOW);  // LED_ON
+        // sleep(1);
+        // digitalWrite(10, HIGH);  // LED_OFF
+        static uint8_t img[65536];  // 受信用バッファ
+        int len = wifiudp.read(img, sizeof(img));
+        M5.Display.drawJpg(img, len);  // 画像表示
+    }
 }
